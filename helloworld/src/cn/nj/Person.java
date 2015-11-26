35327bc1cc67afa4637ae7831255e88dccb0ffc7
gitJava <1316869355@qq.com>
2015-11-26 19:16:00 +0800

    add trace

---
+++ helloworld/src/cn/nj/Person.java
@@ -0,0 +1,26 @@
+public class Person{
+  private String name;
+  private String gender;
+  private int age;
+  
+  public Person(){
+  }
+  public void setName(String name){
+    this.name = name;
+  }
+  public String getName(){
+    return this.name;
+  }
+    public void setGender(String Gender){
+    this.gender = gender;
+  }
+  public String getGender(){
+    return this.gender;
+  }
+    public void setAge(int age){
+    this.age = age;
+  }
+  public String getAge(){
+    return this.age;
+  }
+}
